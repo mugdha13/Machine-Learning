c29480f2b6d51d29d98ebec3cfa1922dad4b6992
Mugdha Deshpande <mugdha.deshpande13@gmail.com>
2018-06-20 15:13:54 +0530

    Create lrCostFunction.m

---
+++ Multi-Class Classification and Neural Networks/lrCostFunction.m
@@ -0,0 +1,18 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+
+J = 0;
+grad = zeros(size(theta));
+
+J = ( (1 / m) * sum(-y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta))) ) + (lambda/(2*m))*sum(theta(2:length(theta)).*theta(2:length(theta))) ;
+
+grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
+
+grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m)*theta(2:length(theta))';
+
+grad = grad(:);
+
+end
